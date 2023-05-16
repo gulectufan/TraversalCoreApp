0a8814cd76649c093cd16c2eeb3fb75731e772c8
Tufan GÜLEÇ <gulectufan@gmail.com>
2023-05-16 04:42:19 +0300

    entity, DAL and business layers are created

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,38 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        // SQL bağlantı adresi oluşturma
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-UJ16HNFH\\SQLEXPRESS; database=TraversalDB; integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Feature2s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
+        // entity'leri DBSET formatında çağırma
+        // veri tipi DbSet<entity_name> olmalı. Biz About kullanacaz.
+        // <About> yazdıktan sonra CTRL + . (Nokta) basılarak bulunduğu konum eklenir.
+        // yani -> using EntityLayer.Concrete eklenir.
+        // Property ismi entity_name'e -s takılmış hali (Abouts) olur!!
+        // bu EntityLayer'daki tüm class'lar için yapılır.
+    }
+}
---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Destination
+    {
+        [Key]
+        public int DestinationID { get; set; } // Gidilecek şehrin ID 'si
+        public string City { get; set; } // Gidilecek şehir
+        public string DayNight { get; set; } // kaç gün kaç gece
+        public double Price { get; set; }   // tur ücreti
+        public string Image { get; set; } // gidilece kşehrin görseli
+        public string Description { get; set; } // turun açıklaması 
+        public int Capacity { get; set; }  // tur kaç kişilik
+        public bool Status { get; set; }  // 
+    }
+}
